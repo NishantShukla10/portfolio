2b3f6eb45b7aaaffa260cb8949877396a953731a
Nishant Shukla <nishantshukla332@gmail.com>
2025-11-11 11:09:44 +0530

    Added stallbidding project

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -1,12 +1,24 @@
-import img1 from "../assets/project/UserFlow.png";
-import img2 from "../assets/project/shoppingCart.png";
-import img3 from "../assets/project/weatherApp.png";
-import img4 from "../assets/project/razorPay.png";
+
+import imgStallBidding from "../assets/project/stallBidding.png"
+import imgUserFlow from "../assets/project/UserFlow.png";
+import imgShoppingCart from "../assets/project/shoppingCart.png";
+import imgWeatherApp from "../assets/project/weatherApp.png";
+import imgRazorPay from "../assets/project/razorPay.png";
 
 export const MyProjects = [
     {
+      title: "Bidding Platform",
+      image: imgStallBidding,
+      description:
+        "Built a secure, role-based bidding system using Node.js, Express, MongoDB, and JWT authentication, enabling principals to create stalls and students to place competitive bids. Designed and implemented RESTful APIs with Mongoose relationships for dynamic bid tracking, real-time highest bid updates, and automated email notifications. Developed a responsive frontend using React and Tailwind CSS with API integration, smooth navigation, and user-centric bid management features.",
+      github: "https://github.com/NishantShukla10/stallBiddingPlatform",
+      live: "https://stallbiddingplatform-qyx1.onrender.com",
+      tags: ["React", "Node.js", "MongoDB", "Express", "Tailwind CSS", "JWT"],
+      color: "from-orange-500 to-red-500"
+    },
+    {
       title: "UserFlow Manager",
-      image: img1,
+      image: imgUserFlow,
       description:
         "Created a MERN stack application with full CRUD functionality, allowing users to be added, edited, deleted, and viewed in detail, with the ability to toggle user status between active and inactive. Implemented dynamic filters (gender, status), sorting, pagination, and search functionality on the homepage to improve user navigation and data accessibility.",
       github: "https://github.com/NishantShukla10/userFlow-Manager",
@@ -16,7 +28,7 @@ export const MyProjects = [
     },
     {
       title: "Shopping Cart",
-      image: img2,
+      image: imgShoppingCart,
       description:
         "I built a fancy online shopping cart with ReactJS and Redux to handle the behind-the-scenes stuff smoothly. Smoothly handles looking at products, picking them, and managing the cart, making shopping easy and enjoyable for everyone. Centralized data management with Redux ensures a smooth user experience",
       github: "https://github.com/NishantShukla10/Shopping-cart",
@@ -26,7 +38,7 @@ export const MyProjects = [
     },
     {
       title: "Weather App",
-      image: img3,
+      image: imgWeatherApp,
       description:
         "Built a weather app using JavaScript to fetch real-time weather data from an API. Implemented dynamic updates to the user interface, ensuring it displays real-time weather information seamlessly. Enhanced the app's functionality by incorporating additional features wind speed, humidity levels, and cloud information.",
       github: "https://github.com/NishantShukla10/Weather-app",
@@ -36,7 +48,7 @@ export const MyProjects = [
     },
     {
       title: "RazorPay UI Clone",
-      image: img4,
+      image: imgRazorPay,
       description:
         "Created a Razorpay UI clone website using HTML and Tailwind CSS. Ensured accurate responsiveness, demonstrating expertise in adaptive and visually appealing web design. Replicated the intuitive and user-friendly interface of Razorpay's payment platform.",
       github: "https://github.com/NishantShukla10/Educational_website",
